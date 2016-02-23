12a91ff3d5d58e4119c08e1be0423c8764c8bd73
Rahul Ghose <rahul@flywheel.com>
2016-02-23 23:25:40 +0530

    deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151129081156) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "ewarels", force: :cascade do |t|
     t.string   "url"
     t.boolean  "is_public"
@@ -44,7 +41,7 @@ ActiveRecord::Schema.define(version: 20151129081156) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
